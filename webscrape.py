972608195485aaf502fa32af07631b1b2ad78551
ruben-hutter <hutter.ruben@gmail.com>
2022-09-03 00:26:13 +0200

    first try to extract downloaded file.

--- webscrape.py
+++ webscrape.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import zipfile
+import os
 
 driver = webdriver.Firefox()
 
 url = "https://www.xcontest.org/world/en/"
+download_path = os.path.join(os.environ['HOME'], "Downloads/")
 
 print("===== Enter login data =====")
 username = input("Username: ")
@@ -14,7 +19,7 @@ print("===== Getting flights =====")
 # open webpage
 driver.get(url)
 # wait to load page
-driver.implicitly_wait(3)
+driver.implicitly_wait(10)
 # enter username & password for login
 login_username = driver.find_element(By.ID, "login-username")
 login_username.send_keys(username)
@@ -22,7 +27,15 @@ login_password = driver.find_element(By.ID, "login-password")
 login_password.send_keys(password)
 login_password.send_keys(Keys.ENTER)
 # go to my_flights
-my_flights = driver.find_element(By.XPATH, "")
+my_flights = driver.find_element(By.LINK_TEXT, "My flights")
+my_flights.click()
 # download all flights
+download_flights = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.LINK_TEXT, "IGC"))
+)
+# extract flights
+with zipfile.ZipFile(download_path+"*.zip", 'r') as zip_ref:
+    zip_ref.extractall(download_path)
 
+#driver.quit()
 print("===== Finished =====")
